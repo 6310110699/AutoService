d0409c203e7487acdec25d32055d8a113866ea6b
NurhayateeMahadunggg <nurhayatee.mhd@gmail.com>
2023-12-07 02:50:39 +0700

    push25

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Signup from './components/register/Signup'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Topbar from './components/topbar/Topbar'
 import Startpage from './components/startpage/Startpage'
 import Login from './components/login/Login'
@@ -29,17 +29,16 @@ function App() {
       <Topbar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
       <Routes>
         <Route path='/' element={<Startpage />}></Route>
-        <Route path='/register' element={<Signup />}></Route>
+        <Route path='/register' element={isLoggedIn ? <Signup /> : <Navigate to='/login' />}></Route>
         <Route path='/login' element={<Login onLogin={() => setIsLoggedIn(true)} />}></Route>
-        <Route path='/home' element={<Home />}></Route>
-        <Route path='/history' element={<History />}></Route>
-        <Route path='/report' element={<Report />}></Route>
-        <Route path='/infomanage' element={<TabView />} />
-        <Route path='/employee' element={<EmployeeManagement />} />
-        <Route path='/spare' element={<SpareManagement />} />
-        <Route path='/carregis' element={<CarRegistration />} />
-        <Route path='/receipt/:customerId' element={<Receipt />} />
-
+        <Route path='/home' element={isLoggedIn ? <Home /> : <Navigate to='/login' />}></Route>
+        <Route path='/history' element={isLoggedIn ? <History /> : <Navigate to='/login' />}></Route>
+        <Route path='/report' element={isLoggedIn ? <Report /> : <Navigate to='/login' />}></Route>
+        <Route path='/infomanage' element={isLoggedIn ? <TabView /> : <Navigate to='/login' />}></Route>
+        <Route path='/employee' element={isLoggedIn ? <EmployeeManagement /> : <Navigate to='/login' />}></Route>
+        <Route path='/spare' element={isLoggedIn ? <SpareManagement /> : <Navigate to='/login' />}></Route>
+        <Route path='/carregis' element={isLoggedIn ? <CarRegistration /> : <Navigate to='/login' />}></Route>
+        <Route path='/receipt/:customerId' element={isLoggedIn ? <Receipt /> : <Navigate to='/login' />}></Route>
       </Routes>
     </BrowserRouter>
   )
