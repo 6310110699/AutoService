fa13ec86ae2a697c6e52b4816b9109007deed4d3
NurhayateeMahadunggg <nurhayatee.mhd@gmail.com>
2024-02-14 04:55:47 +0700

    push61

--- backend/Routes/AuthRoute.js
+++ backend/Routes/AuthRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authController = require('../Controllers/AuthController');
+const authController = require('../controllers/AuthController');
 
 router.post('/login', authController.login);
 router.post('/register', authController.register);
